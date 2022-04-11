f35e3eb5759a775e21600fb82631e7467a1d1fc5
caohyang <caoyiyang@mail.ustc.edu.cn>
2022-04-11 21:47:16 +0800

    update proofofwork.go

--- lab2/template/proofofwork.go
+++ lab2/template/proofofwork.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"math"
-	//"math/big"
+	"math/big"
+	"crypto/sha256"
 )
 
 var (
@@ -24,7 +26,19 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 // Run performs a proof-of-work
 // implement
 func (pow *ProofOfWork) Run() (int, []byte) {
-	nonce := 0
+	nonce := -1
+	
+	// hashdata: []byte
+	hashdata := bytes.Join(pow.block.Data, []byte(""))  
+	hashdata = append(hashdata, pow.block.PrevBlockHash)
+	
+	var final_hashdata []byte 
+	for ; nonce < 0 || Validate(pow) == false ; {
+		nonce += 1
+		hexnonce := IntToHex(nonce)
+		final_hashdata = append(hashdata, hexnonce)
+		pow.block.Hash = sha256.Sum256(final_hashdata)
+	}
 
 	return nonce, pow.block.Hash
 }
@@ -32,5 +46,12 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 // Validate validates block's PoW
 // implement
 func (pow *ProofOfWork) Validate() bool {
-	return true
+	big1 := new(big.Int).SetString(pow.block.Hash, 16)
+    big2 := big.NewInt(1<<(256-pow.block.Bits))
+	result := big1.cmp(big2)
+	if result < 0 {
+		return true
+	} else {
+		return false
+	}
 }
